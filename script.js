330d3d5593c4c684b188f4ab8a7dadd7a0656ba5
lucagrandieri <grandieriluca@gmail.com>
2026-02-02 17:51:13 +0100

    correzzioni tutor e migliorie formatter

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ let kmPercorsi = Number(prompt("Quanti km percorrerai?"));
 let etaPasseggero = Number(prompt("Quanti anni hai?"));
 
 // creo variabile per il prezzo ancora da definire
-let prezzoBiglietto;
+let prezzoBiglietto = kmPercorsi * 0.21;
 
 // controllo le condizioni
 if (etaPasseggero < 18) {
     prezzoBiglietto = (kmPercorsi * 0.21) * 0.80;
 } else if (etaPasseggero >= 65) {
     prezzoBiglietto = (kmPercorsi * 0.21) * 0.60;
-} else {
-    prezzoBiglietto = kmPercorsi * 0.21;
 }
 
+// stampo il prezzo
+console.log("€" + prezzoBiglietto.toFixed(2));
+
+// soluzione alternativa (formatter)
 // creo il formatter
 const formatterPrezzo = new Intl.NumberFormat('it-IT', {
   style: 'currency',
   currency: 'EUR',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
 });
 
 // stampo il prezzo formattato
